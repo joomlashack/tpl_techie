8292dab5cf4721bf06f3387fb67593cf820a5944
Tito Alvarez <augustoalvarez@gmail.com>
2014-01-08 10:02:20 -0600

    Updated Wright Updated copyright and template name

--- code/component.php
+++ code/component.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * @package     Wright
+ * @package     Techie
  * @subpackage  Component File
  *
- * @copyright   Copyright (C) 2005 - 2013 Joomlashack. Meritage Assets.  All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2014 Joomlashack. Meritage Assets.  All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
