ab294d0d812f773b900c99c9d5467d3e8b21e91d
Roberto <carloscontrerasr@gmail.com>
2014-04-02 17:02:59 +0200

    TPLTECHIE-13 #comment Slide configuration and new properties.

--- code/template.php
+++ code/template.php
@@ -245,10 +245,10 @@ defined('_JEXEC') or die('Restricted access');
                     stop_loop               :   0,          // Pauses slideshow on last slide
                     random                  :   0,          // Randomize slide order (Ignores start slide)
                     slide_interval          :   12000,      // Length between transitions
-                    transition              :   2,          // 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
+                    transition              :   3,          // 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
                     transition_speed        :   300,        // Speed of transition
                     new_window              :   1,          // Image links open in new window/tab
-                    pause_hover             :   0,          // Pause slideshow on hover
+                    pause_hover             :   1,          // Pause slideshow on hover
                     keyboard_nav            :   1,          // Keyboard navigation on/off
                     performance             :   1,          // 0-Normal, 1-Hybrid speed/quality, 2-Optimizes image quality, 3-Optimizes transition speed // (Only works for Firefox/IE, not Webkit)
                     image_protect           :   1,          // Disables image dragging and right click with Javascript
@@ -256,11 +256,11 @@ defined('_JEXEC') or die('Restricted access');
                     // Size & Position
                     min_width               :   0,          // Min width allowed (in pixels)
                     min_height              :   0,          // Min height allowed (in pixels)
-                    vertical_center         :   1,          // Vertically center background
-                    horizontal_center       :   1,          // Horizontally center background
+                    vertical_center         :   0,          // Vertically center background
+                    horizontal_center       :   0,          // Horizontally center background
                     fit_always              :   0,          // Image will never exceed browser width or height (Ignores min. dimensions)
                     fit_portrait            :   1,          // Portrait images will not exceed browser height
-                    fit_landscape           :   0,          // Landscape images will not exceed browser width
+                    fit_landscape           :   1,          // Landscape images will not exceed browser width
 
                     // Components
                     slide_links             :   'blank',    // Individual links for each slide (Options: false, 'num', 'name', 'blank')
