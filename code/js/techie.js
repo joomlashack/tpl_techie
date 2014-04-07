937c9f58f1d6fa4090de87bf592e52f6cf30be0c
Roberto <carloscontrerasr@gmail.com>
2014-04-07 10:46:13 +0200

    TPLTECHIE-60 #comment Fixing for the aside elements.

--- code/js/techie.js
+++ code/js/techie.js
@@ -3,12 +3,18 @@ jQuery(function() {
 	function contentHasContent () {
 		jQueryContentChildren = jQuery('#main-content').children();
 		jQueryContentChildren.each(function () {
+			if (jQuery(this).is('aside')) {
+					if (jQuery(this).children().length > 0) {
+						jQuery('#main-content').addClass('content');
+						return;
+					}
+			}
 			jQuerySubChildren = jQuery(this).children();
 			jQuerySubChildren.each(function () {
 				jQueryHasChildren = jQuery(this).children();
-				console.log(jQueryHasChildren);
 			if (jQueryHasChildren.length > 0 )  {
 				jQuery('#main-content').addClass('content');
+				return;
 			}
 			});
 		});
